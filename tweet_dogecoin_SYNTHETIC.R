b081629e0b61ca5966bb28424d5767de86239430
JC Birbuet <63421495+jcbirbuet@users.noreply.github.com>
2021-04-07 11:20:59 -0400

    Update tweet_dogecoin_SYNTHETIC.R

--- tweet_dogecoin_SYNTHETIC.R
+++ tweet_dogecoin_SYNTHETIC.R
@@ -1,4 +1,4 @@
-##### Impacto del Tweet de Musk sobre la cotuzaciÛn de Dogecoin ######
+##### Impacto del Tweet de Musk sobre la cotuzaci√≥n de Dogecoin ######
 library(dplyr)
 library(riingo)
 library(ggplot2)
@@ -12,20 +12,20 @@ library(quantmod)
 # 0 Nos conectamos a las bases de datos de riingo 
 riingo_set_token(token= "a3db2cbda9081225835e5a356e00769f7abe640f", inform = TRUE)
 
-# 1 establecemos las fechas para el an·lisis
+# 1 establecemos las fechas para el an√°lisis
 start_analysis <- as.POSIXct('2021-01-28 11:00:00 UTC', tz = 'UTC')
 end_analysis <- as.POSIXct('2021-01-29 01:00:00 UTC', tz = 'UTC')
 tweet_doge <- as.POSIXct('2021-01-28 22:47:00 UTC', tz = 'UTC')
 #end_analysis <- as.POSIXct('2021-01-29 11:53:00 UTC', tz = 'UTC')
 # 1 Obtenemos los datos de bitcoin (contrafactual) y de dogecoin 
 
-# obtenemos Dogecoin en Bitcoin y luego se convierte en usd 
+# obtenemos Dogecoin en usd 
 doge <- riingo_crypto_prices(
   'dogeusd', start_date = start_analysis,
   end_date = end_analysis, resample_frequency = '1min'
 ) %>% mutate(crypto = 'Dogecoin')
 
-# obtenemos datos de Bitcoin, Litecoin, Cardano en dolares
+# obtenemos datos de Bitcoin, Litecoin, Cardano tambi√©n en usd, estas series se emplear√°n para construir el contrafactual
 
 bit <- riingo_crypto_prices(
     'btcusd', start_date = start_analysis, # si se quiere en euros es btceur 
@@ -48,12 +48,11 @@ ethereum <- riingo_crypto_prices(
 ) %>% mutate(crypto = 'Ethereum')
 
   
-# 2 justamos las bases manteniendo ˙nicamente las columnas donde hay datos tanto de dogecoin como de bitcoin
-# para esto se agrupa los datos por fecha y hora, luego se crea una columna "crypto" que toma valores n = 1 
-# o n = 2, siendo 1 cuando la cotizaciÛn en esa fecha y hora es solo de una de las criptomonedas, y 2 cuando
-# en esa fecha y hora se transaron ambas criptomonedas, luego se filtra la base para n=2 y de esa manera se 
-# obtiene una base que contiene transacciones en fecha y hora exactamente iguales 
-# left_join(x, y, by='Flag') %>% left_join(., z, by='Flag') 
+# 2 juntamos las bases manteniendo √∫nicamente las columnas donde hay datos tanto de dogecoin como de las otras criptomonedas
+# para esto se agrupa los datos por fecha y hora, luego se crea una columna "crypto" que toma valores n = 1 o n = 2, siendo 1 
+# cuando la cotizaci√≥n en esa fecha y hora es solo de una de las criptomonedas, y 2 cuando en esa fecha y hora se transaron 
+# ambas criptomonedas, luego se filtra la base para n=2 y de esa manera se obtiene una base que contiene transacciones en 
+# fecha y hora exactamente iguales.
 
 datos <- full_join(doge, bit) %>% 
   full_join(., lite ) %>% 
@@ -110,16 +109,16 @@ ethereum_norm <- normalizar(datos_final$Ethereum)
 base_norm <- as.data.frame(cbind(doge_norm, bit_norm, lite_norm, cardano_norm, ethereum_norm)) %>%
   mutate(tiempo = as.POSIXct(tiempo))
 
-# 6 Gr·ficos
+# 6 Gr√°ficos
 
-# 6.1 Gr·fico de la serie de tiempo de Dogecoin
+# 6.1 Gr√°fico de la serie de tiempo de Dogecoin
 ggplot(base_norm) + geom_line(aes(tiempo, doge_norm), color = "black") + 
   xlab('Tiempo') +
   ylab('Precio (USD)') +
-  ggtitle('EvoluciÛn de precios de las Dogecoin') +
+  ggtitle('Evoluci√≥n de precios de las Dogecoin') +
   geom_vline(xintercept = tweet_doge)
 
-# 6.2 Gr·fico con todas las series de tiempo
+# 6.2 Gr√°fico con todas las series de tiempo
 ggplot(base_norm) + 
   geom_line(aes(tiempo, doge_norm), color = "black", size = 1.5) + 
   geom_line(aes(tiempo, bit_norm), color = "red") + 
@@ -128,7 +127,7 @@ ggplot(base_norm) +
   geom_line(aes(tiempo, ethereum_norm), color = "#593EA4") + 
     xlab('Tiempo') +
   ylab('Precio (USD)') +
-  ggtitle('EvoluciÛn de precios de las dos criptomonedas') +
+  ggtitle('Evoluci√≥n de precios de las dos criptomonedas') +
   geom_vline(xintercept = tweet_doge)
 
 
